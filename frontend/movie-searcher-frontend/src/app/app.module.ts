bfd216182a83e6ae916b9123880a8d80c6201c02
JJ807 <jakubstrugaa@gmail.com>
2021-12-14 22:37:28 +0100

    build overwrite

--- frontend/movie-searcher-frontend/src/app/app.module.ts
+++ frontend/movie-searcher-frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {CommonModule} from "@angular/common";
+import {CommonModule, HashLocationStrategy, LocationStrategy} from "@angular/common";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {HomeComponent} from "./home/home.component";
@@ -14,7 +14,8 @@ import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
 import {MatDividerModule} from "@angular/material/divider";
-import { PersonComponent } from './home/person/person.component';
+import {PersonComponent} from './home/person/person.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { PersonComponent } from './home/person/person.component';
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     AppRoutingModule,
     MatToolbarModule,
     MatFormFieldModule,
@@ -38,7 +40,12 @@ import { PersonComponent } from './home/person/person.component';
     MatCardModule,
     MatDividerModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
